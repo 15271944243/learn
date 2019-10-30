374753f69e41ad1bf3a532dc99e536fd827b2420
xiaoxiaoxiang <xiaoxiaoxiang@xhqb.com>
2019-10-30 10:17:42 +0800

    add function interface test

--- lambda/src/test/java/xxx/UnaryOperatorTest.java
+++ lambda/src/test/java/xxx/UnaryOperatorTest.java
@@ -5,20 +5,21 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.UnaryOperator;
 
 /**
  * @description:
  * @author: xiaoxiaoxiang
- * @date: 2019/10/26 17:09
+ * @date: 2019/10/29 15:10
  */
-public class PredicateTest {
+public class UnaryOperatorTest {
 
     @Test
-    public void predicateTest() {
+    public void unaryOperatorTest() {
         List<String> features = new ArrayList<>(
                 Arrays.asList("Lambdas", "Default Method",
                         "Stream API", "Date and Time API"));
-        features.removeIf(t -> t.length() > 10);
-        System.out.println(features.size());
+        UnaryOperator unaryOperator = a ->  a + " test";
+        features.replaceAll(unaryOperator);
     }
 }
